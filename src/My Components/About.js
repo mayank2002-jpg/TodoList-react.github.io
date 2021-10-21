a4a2423c4ae04acfe171c5bb516e25ac315db169
Mayank Mahajan <mayankmahajan936@gmail.com>
2021-10-21 22:39:42 +0530

    Todo-List-react
    Build My first React app

---
+++ src/My Components/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is About Component!!
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus corrupti dolor sequi vero molestias a, rerum fuga fugit, cum, architecto nemo natus laboriosam ea vel tenetur earum voluptate deleniti laborum. Ipsam animi ratione quod sint suscipit at nobis, maxime saepe?</p>
+        </div>
+    )
+}
